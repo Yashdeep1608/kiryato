69a0c5ce778b4858e0c7eb53bce1edb58a898e98
Yashdeep1608 <rjyashsainirock24@gmail.com>
2025-08-02 20:50:58 +0530

    Update site

--- mailing/mailingvariables.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $mail_host = "smtp.gmail.com";
-    $mail_port = "587";
-    $mail_sender_email = "yashyosaini00@gmail.com"; //sender
-    $mail_sender_password = "ufpm gcfp xlnf ribm"; //sender
-    $mail_sender_name = "Kriyato Contact Form"; //sender name
-?>
